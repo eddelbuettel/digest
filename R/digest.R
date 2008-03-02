543df6705591e1eea585e829a2f0ba26fedff1ea
Dirk Eddelbuettel <edd@debian.org>
2008-03-02 18:56:57 +0000

    added version 0.3.0 dated 2007-04-25

--- R/digest.R
+++ R/digest.R
@@ -1,72 +1,67 @@
 
-# $Id: digest.R,v 1.9 2007/03/09 03:53:56 edd Exp $
+# $Id: digest.R,v 1.11 2007/03/13 02:49:36 edd Exp edd $
 
  digest <- function(object, algo=c("md5", "sha1", "crc32"), 
-                    serialize=TRUE, file=FALSE, length=Inf, skip=0) {
-  algo <- match.arg(algo)
-  if (is.infinite(length)) {
-    length <- -1               # internally we use -1 for infinite len
-  }
-  if (serialize && !file) {
-    ## next two lines by
-    ## 
-    ## Message-ID: <59d7961d0607280852x386bac20tda7ca2d829da179@mail.gmail.com>
-    ## From: "Henrik Bengtsson" <hb@stat.berkeley.edu>
-    ## To: "r-devel@r-project.org" <r-devel@r-project.org>
-    ## Cc: edd@debian.org
-    ## Subject: Re: Package digest broken under R v2.4.0 devel
-    ## Date: Fri, 28 Jul 2006 08:52:04 -0700
-    ##
-    ## replacing this
-    ##   object <- serialize(object, NULL, TRUE) 
-    ## which doesn't fly with R-devel pre-2.4.0
-    ##
-    object <- serialize(object, connection=NULL, ascii=TRUE)
-    ## object <- paste(object, collapse="")
-
-    ## Message-ID: <59d7961d0701060152k2027d19bi7ab07747f5eed6bf@mail.gmail.com>
-    ## From: "Henrik Bengtsson" <hb@stat.berkeley.edu>
-    ## To: "Dirk Eddelbuettel" <edd@debian.org>
-    ## Subject: digest: rawToChar() instead of paste()
-    ## Date: Sat, 6 Jan 2007 20:52:25 +1100
-    ##
-    ## Hi Dirk,
-    ## 
-    ## while trying to generate md5:s for large R objects I noticed that:
-    ## 
-    ##     object <- serialize(object, connection=NULL, ascii=TRUE);
-    ##     object <- rawToChar(object);
-    ## 
-    ## is faster than and half the size of:
-    ## 
-    ##     object <- serialize(object, connection=NULL, ascii=TRUE);
-    ##     object <- paste(object, collapse="");
-    ## 
-    object <- rawToChar(object)
-  } else if (!is.character(object) && !inherits(object,"raw")) {
-    stop("Argument object must be of type character or raw vector if serialize is FALSE")
-  }
-  if (inherits(object,"raw") && file)
-    stop("file=TRUE cannot be used with a raw object")
-  if (!inherits(object,"raw")) object <- as.character(object)
-  algoint <- switch(algo,
-                    md5=1,
-                    sha1=2,
-                    crc32=3)
-  if (file) {
-    algoint <- algoint+100
-    object <- path.expand(object)
-    if (file.access(object)<0) {
-      stop(c("Can't open input file:", object))
-    }
-  }
-  val <- .Call("digest",
-               object,
-               as.integer(algoint),
-               as.integer(length),
-               as.integer(skip),
-               PACKAGE="digest")
-  return(val)
+                    serialize=TRUE, file=FALSE, length=Inf,
+                    skip="auto", ascii=FALSE) {
+   algo <- match.arg(algo)
+   if (is.infinite(length)) {
+     length <- -1               # internally we use -1 for infinite len
+   }
+   
+   if (is.character(file) && missing(object)) {
+     object <- file
+     file <- TRUE
+   }
+   
+   if (serialize && !file) {
+     object <- serialize(object, connection=NULL, ascii=ascii)
+     ## we support raw vectors, so no mangling of 'object' is necessary
+     ## regardless of R version
+     ## skip="auto" - skips the serialization header [SU]
+     if (any(!is.na(pmatch(skip,"auto")))) {
+       if (ascii) {
+         ## HB 14 Mar 2007:
+         ## Exclude serialization header (non-data dependent bytes but R
+         ## version specific).  In ASCII, the header consists of for rows
+         ## ending with a newline ('\n').  We need to skip these.
+         ## The end of 4th row is *typically* within the first 18 bytes
+         skip <- which(object[1:30] == as.raw(10))[4]
+       } else {
+         skip <- 14
+       }
+       ## Was: skip <- if (ascii) 18 else 14
+     } 
+   } else if (!is.character(object) && !inherits(object,"raw")) {
+     stop("Argument object must be of type character or raw vector if serialize is FALSE")
+   }
+   if (file && !is.character(object))
+     stop("file=TRUE can only be used with a character object")
+   ## HB 14 Mar 2007:  null op, only turned to char if alreadt char
+   ##if (!inherits(object,"raw"))
+   ##  object <- as.character(object)   
+   algoint <- switch(algo,
+                     md5=1,
+                     sha1=2,
+                     crc32=3)
+   if (file) {
+     algoint <- algoint+100
+     object <- path.expand(object)
+     if (file.access(object)<0) {
+       stop(c("Can't open input file:", object))
+     }
+   }
+   ## if skip is auto (or any other text for that matter), we just turn it 
+   ## into 0 because auto should have been converted into a number earlier
+   ## if it was valid [SU]
+   if (is.character(skip)) skip <- 0
+   val <- .Call("digest",
+                object,
+                as.integer(algoint),
+                as.integer(length),
+                as.integer(skip),
+                PACKAGE="digest")
+   return(val)
 }
 
 
