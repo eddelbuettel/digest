dde8ecaed2044f027482b237179574cdfe80a4a6
Dirk Eddelbuettel <edd@debian.org>
2008-03-02 18:52:56 +0000

    added version 0.2.1 dated 2005-11-03

--- R/digest.R
+++ R/digest.R
@@ -1,25 +1,33 @@
 
-# $Id: digest.R,v 1.3 2005/03/15 04:27:29 edd Exp $
+# $Id: digest.R,v 1.4 2005/11/03 05:00:48 edd Exp $
 
-digest <- function(object, algo="md5", serialize=TRUE) {
-  if (serialize)
+ digest <- function(object, algo=c("md5", "sha1", "crc32"), 
+                    serialize=TRUE, file=FALSE, length=Inf) {
+  algo = match.arg(algo)
+  if (is.infinite(length))
+    length=-1;                          # internally we use -1 for infinite len
+  if (serialize && !file)
     object <- serialize(object, NULL, TRUE) # turns object into ascii string
   else if (!is.character(object)) 
     stop("Argument object must be of type character if serialize is FALSE")
 
-  if (is.character(algo)) {
-    algoint <- switch(algo,
-                      md5=1,
-                      sha1=2,
-                      crc32=3)
-    val <- .Call("digest",
-                 as.character(object),
-                 as.integer(algoint),
-                 PACKAGE="digest")
-    return(val)
-  } else {
-    warning("The second argument must be of type character")
+  object = as.character(object)
+  algoint <- switch(algo,
+                    md5=1,
+                    sha1=2,
+                    crc32=3)
+  if (file) {
+    algoint = algoint+100;
+    if (file.access(object)<0)
+      stop(c("Can't open input file:", object))
   }
+  val <- .Call("digest",
+               as.character(object),
+               as.integer(algoint),
+               as.integer(length),
+               PACKAGE="digest")
+  return(val)
 }
+
 
 
--- src/digest.c
+++ src/digest.c
@@ -5,7 +5,7 @@
 
   Copyright 2003, 2004, 2005  Dirk Eddelbuettel <edd@debian.org>
 
-  $Id: digest.c,v 1.4 2005/03/23 03:55:15 edd Exp $
+  $Id: digest.c,v 1.5 2005/11/03 05:04:42 edd Exp $
 
   This file is part of the digest packages for GNU R.
   It is made available under the terms of the GNU General Public
@@ -37,14 +37,16 @@ unsigned long ZEXPORT digest_crc32(unsigned long crc,
 				   const unsigned char FAR *buf,
 				   unsigned len);
 
-SEXP digest(SEXP Txt, SEXP Algo) {
-
+SEXP digest(SEXP Txt, SEXP Algo, SEXP Length) {
+  FILE *fp=0;
   char *txt = STRING_VALUE(Txt);
   int algo = INTEGER_VALUE(Algo);
+  int  length = INTEGER_VALUE(Length);
   SEXP result = NULL;
   char output[41];		/* 33 for md5, 41 for sha1 */
-
-
+  int nChar = strlen(txt);
+  if (length>=0 && length<nChar) nChar = length;
+  
   switch (algo) {
     case 1: {			/* md5 case */
       md5_context ctx;
@@ -52,7 +54,7 @@ SEXP digest(SEXP Txt, SEXP Algo) {
       int j;
 
       md5_starts( &ctx );
-      md5_update( &ctx, (uint8 *) txt, strlen(txt));
+      md5_update( &ctx, (uint8 *) txt, nChar);
       md5_finish( &ctx, md5sum );
 
       for(j = 0; j < 16; j++) {
@@ -66,7 +68,7 @@ SEXP digest(SEXP Txt, SEXP Algo) {
       unsigned char sha1sum[20];
 
       sha1_starts( &ctx );
-      sha1_update( &ctx, (uint8 *) txt, strlen(txt));
+      sha1_update( &ctx, (uint8 *) txt, nChar);
       sha1_finish( &ctx, sha1sum );
 
       for( j = 0; j < 20; j++ ) {
@@ -76,7 +78,7 @@ SEXP digest(SEXP Txt, SEXP Algo) {
     }
     case 3: {			/* crc32 case */
       unsigned long val, l;
-      l = strlen(txt);
+      l = nChar;
 
       val  = digest_crc32(0L, 0, 0);
       val  =  digest_crc32(val , txt,(unsigned) l);
@@ -85,7 +87,82 @@ SEXP digest(SEXP Txt, SEXP Algo) {
 
       break;
     }
-    
+    case 101: {			/* md5 file case */
+      int j;
+      md5_context ctx;
+      unsigned char buf[1024];
+      unsigned char md5sum[16];
+
+      if (!(fp = fopen(txt,"rb"))) {
+        error(strcat("Can not open input file: ", txt));    
+        return(NULL);
+      }
+      md5_starts( &ctx );
+      if (length>=0) {  
+        while( ( nChar = fread( buf, 1, sizeof( buf ), fp ) ) > 0 && length>0) {
+          if (nChar>length) nChar=length;
+          md5_update( &ctx, buf, nChar );
+          length -= nChar;
+        }
+      } else {
+        while( ( nChar = fread( buf, 1, sizeof( buf ), fp ) ) > 0) 
+          md5_update( &ctx, buf, nChar );
+      }
+      fclose(fp);
+      md5_finish( &ctx, md5sum );
+      for(j = 0; j < 16; j++) sprintf(output + j * 2, "%02x", md5sum[j]);
+      break;
+    }
+    case 102: {			/* sha1 file case */
+      int j;
+      sha1_context ctx;
+      unsigned char buf[1024];
+      unsigned char sha1sum[20];
+      
+      if (!(fp = fopen(txt,"rb"))) {
+        error(strcat("Can not open input file: ", txt));    
+        return(NULL);
+      }
+      sha1_starts ( &ctx );
+      if (length>=0) {  
+        while( ( nChar = fread( buf, 1, sizeof( buf ), fp ) ) > 0 && length>0) {
+          if (nChar>length) nChar=length;
+          sha1_update( &ctx, buf, nChar );
+          length -= nChar;
+        }
+      } else {
+        while( ( nChar = fread( buf, 1, sizeof( buf ), fp ) ) > 0) 
+          sha1_update( &ctx, buf, nChar );
+      }
+      fclose(fp);
+      sha1_finish ( &ctx, sha1sum );
+      for( j = 0; j < 20; j++ ) sprintf( output + j * 2, "%02x", sha1sum[j] );
+      break;
+    }
+    case 103: {			/* crc32 file case */
+      unsigned char buf[1024];
+      unsigned long val;
+      
+      if (!(fp = fopen(txt,"rb"))) {
+        error(strcat("Can not open input file: ", txt));    
+        return(NULL);
+      }
+      val  = digest_crc32(0L, 0, 0);
+      if (length>=0) {  
+        while( ( nChar = fread( buf, 1, sizeof( buf ), fp ) ) > 0 && length>0) {
+          if (nChar>length) nChar=length;
+          val  = digest_crc32(val , buf, (unsigned) nChar);
+          length -= nChar;
+        }
+      } else {
+        while( ( nChar = fread( buf, 1, sizeof( buf ), fp ) ) > 0) 
+          val  = digest_crc32(val , buf, (unsigned) nChar);
+      }
+      fclose(fp);      
+      sprintf(output, "%2.2x", (unsigned int) val);
+      break;
+    }
+
     default: {
       error("Unsupported algorithm code");
       return(NULL);
