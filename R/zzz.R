bf475203988f10a29c503b4152b2f2a7aee31936
Dirk Eddelbuettel <edd@debian.org>
2008-03-02 18:44:09 +0000

    added version 0.1.2 dated 2004-05-26

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,5 @@
-# $Id: zzz.R,v 1.1 2003/11/29 01:49:27 edd Exp $
+# $Id: zzz.R,v 1.2 2004/05/27 01:43:40 edd Exp $
 .First.lib <- function(lib, pkg) {
-  if (as.numeric(R.version$minor) < 8)  # need serialize package for R < 1.8.0
-    if (!require(serialize, quiet=TRUE))
-      stop("The required library serialize could not be loaded")
   library.dynam("digest", pkg, lib )
 }
 
