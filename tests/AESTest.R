db212afbf744c929d32b53b8f5293e86da8bebf3
Dirk Eddelbuettel <edd@debian.org>
2013-01-25 14:42:35 +0000

    Switch from paste0() to paste() to permit use on R-oldrelease as per email by Uwe Ligges

--- tests/AESTest.R
+++ tests/AESTest.R
@@ -39,26 +39,26 @@ stopifnot(identical(plaintext, aes$decrypt(aes256, raw=TRUE)))
 
 # SP800-38a examples
 
-plaintext <- hextextToRaw(paste0("6bc1bee22e409f96e93d7e117393172a",
-                                 "ae2d8a571e03ac9c9eb76fac45af8e51", 
-                                 "30c81c46a35ce411e5fbc1191a0a52ef", 
-                                 "f69f2445df4f9b17ad2b417be66c3710"))
+plaintext <- hextextToRaw(paste("6bc1bee22e409f96e93d7e117393172a",
+                                "ae2d8a571e03ac9c9eb76fac45af8e51",
+                                "30c81c46a35ce411e5fbc1191a0a52ef",
+                                "f69f2445df4f9b17ad2b417be66c3710",sep=""))
 key <- hextextToRaw("2b7e151628aed2a6abf7158809cf4f3c")
-                                 
-ecb128output <- hextextToRaw(paste0("3ad77bb40d7a3660a89ecaf32466ef97",
-                                    "f5d3d58503b9699de785895a96fdbaaf",
-                                    "43b1cd7f598ece23881b00e3ed030688",
-                                    "7b0c785e27e8ad3f8223207104725dd4"))
+
+ecb128output <- hextextToRaw(paste("3ad77bb40d7a3660a89ecaf32466ef97",
+                                   "f5d3d58503b9699de785895a96fdbaaf",
+                                   "43b1cd7f598ece23881b00e3ed030688",
+                                   "7b0c785e27e8ad3f8223207104725dd4",sep=""))
 aes <- AES(key)
 ecb128 <- aes$encrypt(plaintext)
 ecb128
 stopifnot(identical(ecb128, ecb128output))
 stopifnot(identical(plaintext, aes$decrypt(ecb128, raw=TRUE)))
 
-cbc128output <- hextextToRaw(paste0("7649abac8119b246cee98e9b12e9197d",
+cbc128output <- hextextToRaw(paste("7649abac8119b246cee98e9b12e9197d",
                                     "5086cb9b507219ee95db113a917678b2",
                                     "73bed6b8e3c1743b7116e69e22229516",
-                                    "3ff1caa1681fac09120eca307586e1a7"))
+                                    "3ff1caa1681fac09120eca307586e1a7",sep=""))
 iv <- hextextToRaw("000102030405060708090a0b0c0d0e0f")
 aes <- AES(key, mode="CBC", IV=iv)
 cbc128 <- aes$encrypt(plaintext)
@@ -66,11 +66,11 @@ cbc128
 stopifnot(identical(cbc128, cbc128output))
 aes <- AES(key, mode="CBC", IV=iv)
 stopifnot(identical(plaintext, aes$decrypt(cbc128, raw=TRUE)))
-                                 
-ctr128output <- hextextToRaw(paste0("874d6191b620e3261bef6864990db6ce",
-                                    "9806f66b7970fdff8617187bb9fffdff",
-                                    "5ae4df3edbd5d35e5b4f09020db03eab",
-                                    "1e031dda2fbe03d1792170a0f3009cee"))
+
+ctr128output <- hextextToRaw(paste("874d6191b620e3261bef6864990db6ce",
+                                   "9806f66b7970fdff8617187bb9fffdff",
+                                   "5ae4df3edbd5d35e5b4f09020db03eab",
+                                   "1e031dda2fbe03d1792170a0f3009cee",sep=""))
 iv <- hextextToRaw("f0f1f2f3f4f5f6f7f8f9fafbfcfdfeff")
 aes <- AES(key, mode="CTR", IV=iv)
 ctr128 <- aes$encrypt(plaintext)
