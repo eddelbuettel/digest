33cac3fb02474ac8ace95128ffb7f3c163e3d008
Dirk Eddelbuettel <edd@debian.org>
2014-12-16 18:41:04 -0600

    remove two semicolons which gcc -pedantic complains about

--- src/xxhash.c
+++ src/xxhash.c
@@ -535,7 +535,7 @@ XXH_errorcode XXH32_freeState(XXH32_state_t* statePtr)
 {
     XXH_free(statePtr);
     return XXH_OK;
-};
+}
 
 XXH64_state_t* XXH64_createState(void)
 {
@@ -546,7 +546,7 @@ XXH_errorcode XXH64_freeState(XXH64_state_t* statePtr)
 {
     XXH_free(statePtr);
     return XXH_OK;
-};
+}
 
 
 /*** Hash feed ***/
