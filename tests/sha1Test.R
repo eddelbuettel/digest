abad6191b955b9a84d75ee7c04c0e232f490ee66
Thierry onkelinx <thierry.onkelinx@inbo.be>
2015-12-29 14:49:20 +0100

    Unit tests for sha1() don't use sha1_digits() and  are more strict. They will fail when the output is incorrect.

--- tests/sha1Test.R
+++ tests/sha1Test.R
@@ -15,54 +15,71 @@ x.dataframe <- data.frame(
 x.matrix.num <- as.matrix(x.numeric)
 x.matrix.letter <- as.matrix(letters)
 x.dataframe.round <- x.dataframe
-x.dataframe.round$Y <- signif(x.dataframe.round$Y, sha1_digits())
+x.dataframe.round$Y <- signif(x.dataframe.round$Y, 14)
 x.factor <- factor(letters)
 
 # tests using detailed numbers
-identical(x.numeric, signif(x.numeric, sha1_digits())) == FALSE
-identical(x.matrix.num, signif(x.matrix.num, sha1_digits())) == FALSE
+stopifnot(
+    !identical(x.numeric, signif(x.numeric, 14))
+)
+stopifnot(
+    !identical(x.matrix.num, signif(x.matrix.num, 14))
+)
 # returns the correct SHA1
 x.hex <- sprintf("%a", x.numeric)
 exponent <- as.integer(gsub("^.*p", "", x.hex))
-zapsmall.hex <- floor(log2(10 ^ -sha1_digits("zapsmall")))
-digits.hex <- ceiling(log(10 ^ sha1_digits("base"), base = 16))
+zapsmall.hex <- floor(log2(10 ^ -7))
+digits.hex <- ceiling(log(10 ^ 14, base = 16))
 mantissa <- gsub(".*x1\\.{0,1}", "",x.hex)
 mantissa <- gsub("p.*$", "", mantissa)
 mantissa <- substring(mantissa, 1, digits.hex)
 mantissa <- gsub("0$", "", mantissa)
 negative <- ifelse(grepl("^-", x.hex), "-", "")
-identical(
-    sha1(x.numeric),
-    sha1(paste0(negative, mantissa, " ", exponent))
-)
-identical(
-    sha1(letters),
-    digest(letters, algo = "sha1")
-)
-identical(
-    sha1(x.list),
-    digest(
-        sapply(x.list, sha1),
-        algo = "sha1"
+stopifnot(
+    identical(
+        sha1(x.numeric),
+        sha1(paste0(negative, mantissa, " ", exponent))
     )
 )
-identical(
-    sha1(x.dataframe),
-    digest(sapply(x.dataframe, sha1), algo = "sha1")
+stopifnot(
+    identical(
+        sha1(letters),
+        digest(letters, algo = "sha1")
+    )
 )
-identical(
-    sha1(x.matrix.num),
-    sha1(as.vector(x.matrix.num))
+stopifnot(
+    identical(
+        sha1(x.list),
+        digest(
+            sapply(x.list, sha1),
+            algo = "sha1"
+        )
+    )
 )
-identical(
-    sha1(x.matrix.letter),
-    digest(x.matrix.letter, algo = "sha1")
+stopifnot(
+    identical(
+        sha1(x.dataframe),
+        digest(sapply(x.dataframe, sha1), algo = "sha1")
+    )
 )
-identical(
-    sha1(x.factor),
-    digest(x.factor, algo = "sha1")
+stopifnot(
+    identical(
+        sha1(x.matrix.num),
+        sha1(as.vector(x.matrix.num))
+    )
 )
-
+stopifnot(
+    identical(
+        sha1(x.matrix.letter),
+        digest(x.matrix.letter, algo = "sha1")
+    )
+)
+stopifnot(
+    identical(
+        sha1(x.factor),
+        digest(x.factor, algo = "sha1")
+    )
+)
 
 lm.model.0 <- lm(weight ~ Time, data = ChickWeight)
 lm.model.1 <- lm(weight ~ 1, data = ChickWeight)
@@ -79,36 +96,42 @@ anova.list <- list(
 z <- apply(
     anova.list[["lm"]],
     1,
-    num2hex,
-    digits = sha1_digits("coef"),
-    zapsmall = sha1_digits("zapsmall")
+    sha1,
+    digits = 4,
+    zapsmall = 7
 )
-identical(
-    sha1(anova.list[["lm"]]),
-    sha1(z)
+stopifnot(
+    identical(
+        sha1(anova.list[["lm"]]),
+        sha1(z)
+    )
 )
 # works with glm anova"
 z <- apply(
     anova.list[["glm"]],
     1,
-    num2hex,
-    digits = sha1_digits("coef"),
-    zapsmall = sha1_digits("zapsmall")
+    sha1,
+    digits = 4,
+    zapsmall = 7
 )
-identical(
-    sha1(anova.list[["glm"]]),
-    sha1(z)
+stopifnot(
+    identical(
+        sha1(anova.list[["glm"]]),
+        sha1(z)
+    )
 )
 z <- apply(
     anova.list[["glm.test"]],
     1,
-    num2hex,
-    digits = sha1_digits("coef"),
-    zapsmall = sha1_digits("zapsmall")
+    sha1,
+    digits = 4,
+    zapsmall = 7
 )
-identical(
-    sha1(anova.list[["glm.test"]]),
-    sha1(z)
+stopifnot(
+    identical(
+        sha1(anova.list[["glm.test"]]),
+        sha1(z)
+    )
 )
 
 test.element <- list(
@@ -141,50 +164,50 @@ test.element <- c(
 
 cat("\ncorrect <- c(\n")
 cat(
-    sprintf("  \"%s\"", sapply(test.element, sha1)),
+    sprintf("    \"%s\"", sapply(test.element, sha1)),
     sep = ",\n"
 )
 cat(")\n")
 correct <- c(
     "8d9c05ec7ae28b219c4c56edbce6a721bd68af82",
     "0df9019fab513592066cc292d412b9054575d844",
-    "f374b4a83af21bb028483fd33dfa811aca7abb96",
+    "37dadeab8d8ce7611f230f9524c1e8ab751c4a6a",
     "37dadeab8d8ce7611f230f9524c1e8ab751c4a6a",
     "37dadeab8d8ce7611f230f9524c1e8ab751c4a6a",
     "ad8d56a358f1c91717e506012ea43a9b700a8d51",
     "0f0714f1142eed0701166aa2d6dcdd798c8420e6",
-    "49731da30df853ee8959035e3e309df129ad5348",
+    "5db33863310fda479e9367aa8b69e339565fb312",
     "5db33863310fda479e9367aa8b69e339565fb312",
     "1f9928593251410322823fefea8c3ef79b4d0254",
     "c799247ef7cc5eb0a3544aa1aef1039e270579a4",
     "692ff1b9390cfc01625d8dbb850d04426e193889",
-    "f4403adc0f5e5e270450150646a7ad0652783047",
+    "4a07275045c4b13d1ffa085be774343227c05f7c",
     "051deeb6fb87e61a7a5a35c80e3b8351f1531ec3",
     "3bc1c85261b958307340b7a8a9fcff3e2586516b",
     "3c23872b9b4e17b16c1d640fe3c29f251202b012",
     "0fc188b4fd874e5ea411a241feb0e247faada054",
     "188710fe63fedb3f4637db5eeb2ecdbc824aa179",
-    "f3863e2ce42f3507b0d65b013b70c84804e246ef",
+    "5c251c1a5a3113b9261ec12aaef062a994d908c8",
     "78d23c7b572a943f697584705cc9514d32269bfd",
     "25228aa01875f7c88b51c299a332c6bd82257d06",
     "51fe9849f2b30d02c73cd7870d5d9b3a19e83654",
     "c165458381d503502e811a153f262fe6a1dfa55e",
     "692ff1b9390cfc01625d8dbb850d04426e193889",
-    "f4403adc0f5e5e270450150646a7ad0652783047",
+    "4a07275045c4b13d1ffa085be774343227c05f7c",
     "051deeb6fb87e61a7a5a35c80e3b8351f1531ec3",
     "3bc1c85261b958307340b7a8a9fcff3e2586516b",
     "3c23872b9b4e17b16c1d640fe3c29f251202b012",
     "0fc188b4fd874e5ea411a241feb0e247faada054",
-    "49731da30df853ee8959035e3e309df129ad5348",
-    "d1fafdecb299e9f16ba224ecb4a0601fd31859f5",
-    "cac51e723748aae54d21b79b40b48f9000f5e90e",
-    "91619e26fbae8cd58c353ee18689728c0c87d002",
-    "53371b092b9b535e58350b4a8078417c9a2419a1",
-    "19a58a3f233de56000e1a1f66ef538904cf3bebe",
-    "20864973f3a271ea8b27e2c75face8663d1a900c",
-    "980927373c7acbe1cf33fa98f653927da59f70b6",
-    "f5c3d80ad7d3253be3309ec910508a6e56c2f178",
-    "8c420b12a14e1ed1f348fee29e864544147c8796",
+    "5db33863310fda479e9367aa8b69e339565fb312",
+    "7fb7dc4afcf762daf6636854257587a1c8e7b144",
+    "653a174a607115158c524d67f96d859ea9d3a6a0",
+    "1d07a67017c81f3ac4110b4c1ad953cf41fed47a",
+    "a5844210ce302594bd43ed7465c67278218172c3",
+    "f8d8e7894c30b7c3ab386a11a554a7dead9d398b",
+    "5e7432ffb37cd554faf8536dcecc4bedfa72a1e8",
+    "7655592ceb37d4f920cb2dfd9e242044d5acdf29",
+    "d9c186396c5a79390758618b8358d9563029e960",
+    "710ded514e2bf24f2703c031577d5b5e10c1963a",
     "00145b50b536d59ade3c6302593fc8f75db50782",
     "64347b1b92bb235b29dcc7dc781fc483179c808f",
     "c0b3c2ed33d9e88da6a73fa6956915c70ef38a20",
@@ -215,27 +238,33 @@ correct <- c(
     "ef60fa66262167e7a31398b16fa762151c6d1b28",
     "a235e3cc7109def777a99e660b9829cea48ce9a4",
     "d19d82f849bad81a39da932d3087a60c78de82c1",
-    "d7ce66852e854bbc6f030156217a93b0e72e83fb",
-    "c7174dc1ad72405e92c04bf35f3c4c602630ae8a",
-    "d26ac1f5fedad77ca54e4692a7ddc0d387bd3130"
+    "44b53cab69e59d93951b81e66bb0f00a87881580",
+    "6094e12d3db414c197b78ed449e78f8ab65e5fb6",
+    "c2d0cf902c070f0b6d33f2b6079155461d56a2e3"
 )
 
 # returns the same SHA1 on both 32-bit and 64-bit OS"
 for (i in seq_along(test.element)) {
-    identical(
-        sha1(test.element[[i]]),
-        correct[i]
+    stopifnot(
+        identical(
+            sha1(test.element[[i]]),
+            correct[i]
+        )
     )
 }
 
 # calculates the SHA1 of a list as the SHA1 of a vector of SHA1 of each element
 this.list <- list("a", "b")
-identical(
-    sha1(this.list),
-    sha1(sapply(this.list, sha1))
+stopifnot(
+    identical(
+        sha1(this.list),
+        sha1(sapply(this.list, sha1))
+    )
 )
 this.list <- list(letters, this.list)
-identical(
-    sha1(this.list),
-    sha1(sapply(this.list, sha1))
+stopifnot(
+    identical(
+        sha1(this.list),
+        sha1(sapply(this.list, sha1))
+    )
 )
