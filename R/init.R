e655bacaf3652e200549bb7c96a26dc4f8c16ca2
Dirk Eddelbuettel <edd@debian.org>
2017-12-13 07:05:08 -0600

    support serialize version via option() and argument

---
+++ R/init.R
@@ -0,0 +1,30 @@
+
+##  digest -- hash digest functions for R
+##
+##  Copyright (C) 2003 - 2017  Dirk Eddelbuettel <edd@debian.org>
+##
+##  This file is part of digest.
+##
+##  digest is free software: you can redistribute it and/or modify
+##  it under the terms of the GNU General Public License as published by
+##  the Free Software Foundation, either version 2 of the License, or
+##  (at your option) any later version.
+##
+##  digest is distributed in the hope that it will be useful,
+##  but WITHOUT ANY WARRANTY; without even the implied warranty of
+##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##  GNU General Public License for more details.
+##
+##  You should have received a copy of the GNU General Public License
+##  along with digest.  If not, see <http://www.gnu.org/licenses/>.
+
+.pkgenv <- new.env(parent=emptyenv())
+
+.onLoad <- function(libname, pkgname) {
+    ## we set a default level of two, with a possible override
+    .pkgenv[["serializeVersion"]] <- getOption("serializeVersion", 2L)
+}
+
+.getSerializeVersion <- function() {
+    getOption("serializeVersion", .pkgenv[["serializeVersion"]])
+}
