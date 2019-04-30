f9eb54ad4ec2368c01b393a337462678bb255f63
Kendon Bell <kmb56@berkeley.edu>
2019-04-30 18:36:50 +1200

    add depends on fastdigest to see if Travis works

--- tests/spookyTest.R
+++ tests/spookyTest.R
@@ -2,8 +2,6 @@
 ## tests for digest spooky
 
 stopifnot(require(digest))
-# Not sure if this is really bad practice. what's the best way to include a package that's just for testing?
-if (!require("fastdigest")) install.packages("fastdigest", repos = "https://cran.rstudio.com")
 
 ## test vectors (originally for md5)
 spookyInput <-
