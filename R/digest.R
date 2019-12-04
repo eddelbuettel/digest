0c9a3e3d4ec198fcc07ec6f435be56e49064c202
Dirk Eddelbuettel <edd@debian.org>
2019-12-04 11:21:01 -0600

    cache the 'are we on Windows' test (closes #137)

--- R/digest.R
+++ R/digest.R
@@ -38,8 +38,6 @@ digest <- function(object, algo=c("md5", "sha1", "crc32", "sha256", "sha512",
     algo <- match.arg(algo)
     errormode <- match.arg(errormode)
 
-    isWindows <- Sys.info()[["sysname"]] == "Windows"
-
     if (is.infinite(length)) {
         length <- -1               # internally we use -1 for infinite len
     }
@@ -93,7 +91,7 @@ digest <- function(object, algo=c("md5", "sha1", "crc32", "sha256", "sha512",
     if (file) {
         algoint <- algoint+100
         object <- path.expand(object)
-        if (isWindows) object <- enc2utf8(object)
+        if (.isWindows()) object <- enc2utf8(object)
         check_file(object, errormode)
     }
     ## if skip is auto (or any other text for that matter), we just turn it
