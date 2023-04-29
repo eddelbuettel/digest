ad6c30007e679db7b578cbd454c142c7566e8db4
Dirk Eddelbuettel <edd@debian.org>
2023-04-29 17:55:20 -0500

    Add (parts of) crc32c implementation by Google
    See https://github.com/google/crc32c
    This replaces the version from Firefox added earlier

--- src/crc32c.h
+++ src/crc32c.h
@@ -1,26 +1,89 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+/* Copyright 2017 The CRC32C Authors. All rights reserved.
+   Use of this source code is governed by a BSD-style license that can be
+   found in the LICENSE file. See the AUTHORS file for names of contributors. */
 
-#ifndef crc32c_h
-#define crc32c_h
+#ifndef CRC32C_CRC32C_H_
+#define CRC32C_CRC32C_H_
 
-#include <stdint.h>
+/* The API exported by the CRC32C project. */
+
+#if defined(__cplusplus)
+
+#include <cstddef>
+#include <cstdint>
+#include <string>
+
+#else  /* !defined(__cplusplus) */
+
 #include <stddef.h>
+#include <stdint.h>
 
-#ifdef __cplusplus
+#endif  /* !defined(__cplusplus) */
+
+
+/* The C API. */
+
+#if defined(__cplusplus)
 extern "C" {
-#endif
+#endif  /* defined(__cplusplus) */
 
-// Compute a CRC32c as defined in RFC3720.  This is a different polynomial than
-// what is used in the crc for zlib, etc.  Typical usage to calculate a new CRC:
-//
-//    ComputeCrc32c(~0, buffer, bufferLength);
-//
-uint32_t ComputeCrc32c(uint32_t aCrc, const void* aBuf, size_t aSize);
+/* Extends "crc" with the CRC32C of "count" bytes in the buffer pointed by
+   "data" */
+uint32_t crc32c_extend(uint32_t crc, const uint8_t* data, size_t count);
+
+/* Computes the CRC32C of "count" bytes in the buffer pointed by "data". */
+uint32_t crc32c_value(const uint8_t* data, size_t count);
 
 #ifdef __cplusplus
-}  // extern "C"
-#endif
+}  /* end extern "C" */
+#endif  /* defined(__cplusplus) */
 
-#endif  // crc32c_h
+
+/* The C++ API. */
+
+#if defined(__cplusplus)
+
+namespace crc32c {
+
+// Extends "crc" with the CRC32C of "count" bytes in the buffer pointed by
+// "data".
+uint32_t Extend(uint32_t crc, const uint8_t* data, size_t count);
+
+// Computes the CRC32C of "count" bytes in the buffer pointed by "data".
+inline uint32_t Crc32c(const uint8_t* data, size_t count) {
+  return Extend(0, data, count);
+}
+
+// Computes the CRC32C of "count" bytes in the buffer pointed by "data".
+inline uint32_t Crc32c(const char* data, size_t count) {
+  return Extend(0, reinterpret_cast<const uint8_t*>(data), count);
+}
+
+// Computes the CRC32C of the string's content.
+inline uint32_t Crc32c(const std::string& string) {
+  return Crc32c(reinterpret_cast<const uint8_t*>(string.data()),
+                string.size());
+}
+
+}  // namespace crc32c
+
+#if __cplusplus > 201402L
+#if __has_include(<string_view>)
+#include <string_view>
+
+namespace crc32c {
+
+// Computes the CRC32C of the bytes in the string_view.
+inline uint32_t Crc32c(const std::string_view& string_view) {
+  return Crc32c(reinterpret_cast<const uint8_t*>(string_view.data()),
+                string_view.size());
+}
+
+}  // namespace crc32c
+
+#endif  // __has_include(<string_view>)
+#endif  // __cplusplus > 201402L
+
+#endif  /* defined(__cplusplus) */
+
+#endif  // CRC32C_CRC32C_H_
